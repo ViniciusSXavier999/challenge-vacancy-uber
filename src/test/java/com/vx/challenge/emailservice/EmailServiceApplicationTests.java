bf0199c88a9ffffd820b8b03560088671122607b
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-01-18 14:50:47 -0300

    initial project structure

---
+++ src/test/java/com/vx/challenge/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vx.challenge.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
